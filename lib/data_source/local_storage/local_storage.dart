63d8e359d82d340ea55a03eb97ba5c10897839eb
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2023-03-13 15:44:54 +0100

    feat: removed redundant files;

--- lib/data_source/local_storage/local_storage.dart
+++ lib/data_source/local_storage/local_storage.dart
@@ -1,9 +1,1 @@
-import 'package:boring_counter/domain/session/session.dart';
-
-abstract class LocalStorage {
-  Future<void> storeSession({
-    required Session? session,
-  });
-
-  Future<Session?> retrieveSession();
-}
+abstract class LocalStorage {}
--- lib/data_source/local_storage/mock_local_storage.dart
+++ lib/data_source/local_storage/mock_local_storage.dart
@@ -1,19 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:boring_counter/data_source/local_storage/local_storage.dart';
-import 'package:boring_counter/domain/session/session.dart';
 
 @Injectable(as: LocalStorage)
 class MockLocalStorage implements LocalStorage {
-  static const _session = Session(
-    accessToken: 'accessToken',
-    refreshToken: 'refreshToken',
-  );
-
-  @override
-  Future<Session?> retrieveSession() async => _session;
-
-  @override
-  Future<void> storeSession({required Session? session}) async {
-    // MARK: expected to do nothing
-  }
 }
