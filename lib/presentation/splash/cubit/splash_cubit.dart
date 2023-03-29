9a6e4e25577e02c1aca407ed43317da76cf0fe9c
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2023-03-29 13:03:28 +0200

    feat: remove few redundant comments;

--- lib/presentation/splash/cubit/splash_cubit.dart
+++ lib/presentation/splash/cubit/splash_cubit.dart
@@ -6,7 +6,6 @@ import 'package:injectable/injectable.dart';
 class SplashCubit extends Cubit<SplashState> {
   SplashCubit() : super(const SplashState.initial());
 
-  // TODO(all): add other thing to array for parallel execution
   Future<void> initializeApp() async {
     emit(const SplashState.loading());
     await Future.wait(
@@ -18,13 +17,11 @@ class SplashCubit extends Cubit<SplashState> {
     emit(const SplashState.ready());
   }
 
-  // TODO(all): await other thing is method for sequential execution
   Future<void> _initializeAllThings() async {
     await _initializeFirstThing();
     await _initializeSecondThing();
   }
 
-  // TODO(all): rename method name & implement method
   Future<void> _initializeFirstThing() async {
     await Future.delayed(
       const Duration(
@@ -34,7 +31,6 @@ class SplashCubit extends Cubit<SplashState> {
     );
   }
 
-  // TODO(all): rename method name & implement method
   Future<void> _initializeSecondThing() async {}
 
   Future<void> _minSplashTime() => Future.delayed(
